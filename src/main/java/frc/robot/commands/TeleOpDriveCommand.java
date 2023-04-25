8c6405141f556771692dd366703562e955a6daf1
VimalBuckley <92557966+VimalBuckley@users.noreply.github.com>
2023-04-25 10:25:41 -0500

    Remove Turn Only Mode

--- src/main/java/frc/robot/commands/TeleOpDriveCommand.java
+++ src/main/java/frc/robot/commands/TeleOpDriveCommand.java
@@ -7,37 +7,15 @@ import frc.robot.subsystems.TankDriveSubsystem;
 public class TeleOpDriveCommand extends CommandBase{
     private TankDriveSubsystem m_tankDrive;
     private CommandXboxController m_controller;
-    private DriveMode mode;
     public TeleOpDriveCommand(TankDriveSubsystem tankDrive, CommandXboxController controller) {
         m_tankDrive = tankDrive;
         m_controller = controller;
-        mode = DriveMode.Normal;
     }
 
     public void execute() {
-        setCase();
-        switch (mode) {
-            case TurnOnly:
-                m_tankDrive.setLeftOutput(m_controller.getRightX());
-                m_tankDrive.setRightOutput(-m_controller.getRightX());
-                break;
-            default:
-                m_tankDrive.setLeftOutput(-m_controller.getLeftY());
-                m_tankDrive.setRightOutput(-m_controller.getRightY());
-        }
+        m_tankDrive.setLeftOutput(-m_controller.getLeftY());
+        m_tankDrive.setRightOutput(-m_controller.getRightY());
     }
 
-    public void setCase() {
-        if (Math.abs(m_controller.getLeftX()) > 0) {
-            mode = DriveMode.TurnOnly;
-        } else {
-            mode = DriveMode.Normal;
-        }
-    }
-
-    public enum DriveMode {
-        Normal,
-        TurnOnly
-    }
 
 }
